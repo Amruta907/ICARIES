d053275555ce43ce7066e163eb1df33327cd0558
Shravani-26-05 <dahakeshravani@gmail.com>
2026-03-18 15:02:55 +0530

    updated

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // Add your Render URL here:
+    host: true,
+    port: 3000,
+    strictPort: true,
+    preview: {
+      allowedHosts: ["icarie-2.onrender.com", "localhost"],
+    },
+  },
+});
